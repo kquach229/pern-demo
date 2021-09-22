ab9a16acbc2642413a4613284680b64c39491c86
kquach229 <kquach229>
2021-09-22 01:51:52 -0400

    initial commit

---
+++ client/src/components/ListTodo.js
@@ -0,0 +1,67 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import EditTodo from './EditTodo';
+
+const ListTodo = () => {
+
+  const [todos, setTodos] = useState([]);
+
+    const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/todos");
+      const jsonData =  await response.json();
+      setTodos(jsonData);
+    } catch(err) {
+      console.log(err.message);
+    }
+  }
+
+  const onClickDelete = async(id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+      method: 'DELETE'
+    });
+    const filteredTodos = todos.filter(todo => todo.todo_id !== id);
+    setTodos(filteredTodos);
+    } catch(err) {
+      console.log(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getTodos();
+  },[]);
+
+
+  return (
+    <Fragment>
+      <table className="table mt-5 text-center">
+    <thead>
+      <tr>
+        <th>description</th>
+        <th>edit</th>
+        <th>delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      {todos.map(todo => {
+        return (
+          <tr key={todo.todo_id}>
+            <td>{todo.description}</td>
+            <td>
+              <EditTodo todo={todo} />
+            </td>
+            <td>
+              <button className="btn btn-danger" onClick={() => onClickDelete(todo.todo_id)}>Delete</button>
+            </td>
+          </tr>
+        )
+      })}
+      
+    </tbody>
+  </table>
+    </Fragment>
+  )
+}
+
+export default ListTodo;
+;
